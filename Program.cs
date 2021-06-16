0a615d193d0cb205ea3b9f12e1c41f948ec759d4
lucaslgr <lucaslgr1206@gmail.com>
2021-06-16 18:07:02 -0300

    Implemented the method GetUserOption in Program.cs

--- Program.cs
+++ Program.cs
@@ -8,5 +8,25 @@ namespace DIO.Series
         {
             Console.WriteLine("Hello World!");
         }
+
+        private static string GetUserOption()
+        {
+            Console.WriteLine();
+            Console.WriteLine("--- DIO Séries ---");
+            Console.WriteLine("Informe a opção desejada: ");
+
+            Console.WriteLine("1 - Listar séries");
+            Console.WriteLine("2 - Inserir nova série");
+            Console.WriteLine("3 - Atualizar série");
+            Console.WriteLine("4 - Excluir série");
+            Console.WriteLine("5 - Visualizar série");
+            Console.WriteLine("6 - Limpar tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string userOption = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return userOption;
+        }
     }
 }
