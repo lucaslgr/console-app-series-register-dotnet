2ac740db4fee3bf66016237ae28ade3eb8539445
lucaslgr <lucaslgr1206@gmail.com>
2021-06-16 17:20:27 -0300

    Implemented the Enum that define the types of series

---
+++ Enum/Gender.cs
@@ -0,0 +1,16 @@
+public enum Name
+{
+    Action = 1,
+    Adventure = 2,
+    Comedy = 3,
+    Documentary = 4,
+    Drama = 5,
+    Espionage = 6,
+    Western = 7,
+    Fantasy = 8,
+    ScienceFiction = 9,
+    Musical = 10,
+    Romance = 11,
+    Triller = 12,
+    Horror = 13,
+}
