77f85dc8cc7c4105798206677d6de0135848d8b6
lucaslgr <lucaslgr1206@gmail.com>
2021-06-16 17:15:45 -0300

    Changed the name of members from portuguese to english

--- classes/Serie.cs
+++ classes/Serie.cs
@@ -3,9 +3,9 @@ namespace DIO.Series
     public class Serie : BaseEntity
     {
         private Gender Gender { get; set; }
-        private string Titulo { get; set; }
-        private string Descricao { get; set; }
-        private int Ano { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private int Year { get; set; }
 
     }
 }
